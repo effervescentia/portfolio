150cdca204dbed3ce39f05ce9fd2cf26f664a696
Ben Teichman <ben.teichman@groupbyinc.com>
2018-08-19 14:02:21 -0400

    baseline

--- src/code-button.ts
+++
@@ -1,7 +0,0 @@
-export class CodeButton {
-  clicked(element: Element) {
-    element.classList.toggle('active');
-    document.querySelector('.fl-splash.outer').classList.toggle('hidden');
-    document.querySelector('.fl-splash.inner').classList.toggle('hidden');
-  }
-}
